8efd53a8e5ff42580434ebf137bc6537f0c07007
BTopchiyski <topchiisky@gmail.com>
2021-11-13 16:12:37 +0200

    Initializing project

---
+++ src/main/java/bg/healthcheck/BIYD/BiydApplication.java
@@ -0,0 +1,13 @@
+package bg.healthcheck.BIYD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiydApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/healthcheck/BIYD/BiydApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.healthcheck.BIYD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiydApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
